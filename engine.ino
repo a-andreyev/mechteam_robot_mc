13a2fcd0ab408d1a6937d648927f7594ec483058
Alexey Andreyev <yetanotherandreyev@gmail.com>
2015-02-25 21:56:12 +0300

    move_servos()

--- engine.ino
+++ engine.ino
@@ -23,6 +23,7 @@ int pos = 0; // variable to store the servo position
  
 void setup() 
 {
+	Serial.begin(9600);
 	my_servo_a.attach(A_PIN); // attaches the servo on pin to the servo object
 	my_servo_b.attach(B_PIN); // attaches the servo on pin to the servo object
 	my_servo_c.attach(C_PIN); // attaches the servo on pin to the servo object
@@ -32,12 +33,58 @@ void setup()
 	//my_servo_g.attach(G_PIN); // attaches the servo on pin to the servo object
 	//my_servo_h.attach(H_PIN); // attaches the servo on pin to the servo object
 } 
+void move_servos(int delta_a=0,int delta_b=0,int delta_c=0,int delta_d=0, int delta_e=0,int delta_f=0,int delta_g=0,int delta_h=0)
+{
+	int a=180/delta_a;
+	int b=180/delta_b;
+	int c=180/delta_c;
+	int d=180/delta_d;
+	int e=180/delta_e;
+	int f=180/delta_f;
+	//
+	//
+	for(int i=0;i<180;i+=1)
+	{
+		if ((delta_a>0)&&(delta_a<180)&&(i%a))
+		{
+			my_servo_a.write(i/a);
+			Serial.write(i/a);
+		}
+		if ((delta_b>0)&&(delta_b<180)&&(i%b))
+		{
+			my_servo_b.write(i/b);
+		}
+		if ((delta_c>0)&&(delta_c<180)&&(i%c))
+		{
+			my_servo_c.write(i/c);
+		}
+		if ((delta_d>0)&&(delta_d<180)&&(i%d))
+		{
+			my_servo_d.write(i/d);
+		}
+		if ((delta_e>0)&&(delta_e<180)&&(i%e))
+		{
+			my_servo_e.write(i/e);
+		}
+		if ((delta_f>0)&&(delta_f<180)&&(i%f))
+		{
+			my_servo_f.write(i/f);
+		}
+		/*
+		if ((delta_g>0)&&(delta_g<180)&&(i%g))
+		{
+			my_servo_g.write(i/g);
+		}
+		if ((delta_h>0)&&(delta_h<180)&&(i%h))
+		{
+			my_servo_h.write(i/h);
+		}
+		*/
+		delay(2);
+	}
+}
  
 void loop() 
 { 
-  for(pos = 0; pos < 180; pos += 1) // goes from 0 degrees to 180 degrees 
-  { // in steps of 1 degree 
-    my_servo_a.write(pos); // tell servo to go to position in variable 'pos' 
-    delay(3); // waits 15ms for the servo to reach the position 
-  }
+  move_servos(10,10,90,90,90,90);
 }
